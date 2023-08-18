b9a5df75b54d5a2d2eab89b0039a8e6120516ddc
Anton Folomkin <afolomkin@croc.ru>
2023-08-18 10:35:33 +0300

    Kafka producer

--- README.md
+++
@@ -1,2 +0,0 @@
-# KafkaMessengerHttp
-Kafka service which helps to send messages with http
---
+++ src/main/java/net/javaguides/springbootkafkatutorial/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,29 @@
+package net.javaguides.springbootkafkatutorial;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("topic1")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+
+	@KafkaListener(id = "muId", topics = "topic1")
+	public void listen(String in) {
+		System.out.println(in);
+	}
+}
